31593c3d10f66f006b5ac68b82d9d7b0dae18596
Jake Lee <jake.lee@fdmgroup.com>
2021-09-22 18:04:21 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    print_text()
+
+def print_text():
+    print("In a hole in the ground there lived a method")
+
 
 if __name__ == '__main__':
     main()
